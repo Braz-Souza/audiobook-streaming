16a07090499faed3208f2441aeb175d602b5f9aa
bz <mail@braz.one>
2025-11-30 23:29:53 -0300

    feat(rtppacket): complete encoding

--- RtpPacket.py
+++ RtpPacket.py
@@ -13,15 +13,40 @@ class RtpPacket:
 		timestamp = int(time())
 		header = bytearray(HEADER_SIZE)
 		#--------------
-		# TO COMPLETE
+		# COMPLETED
 		#--------------
 		# Fill the header bytearray with RTP header fields
 		
-		# header[0] = ...
-		# ...
+		# V=2
+		header[0] = version << 6
+		# P
+		header[0] = padding << 5
+		# E
+		header[0] = extension << 4
+		# CC
+		header[0] = cc & 0x0F
+		# M
+		header[1] = marker << 7
+		# PT
+		header[1] = pt & 0x7F
+		# SEQUENCE_NUMBER
+		header[2] = (seqnum >> 8) & 0xFF
+		header[3] = seqnum & 0xFF
+		# TIMESTAMP
+		header[4] = (timestamp >> 24) & 0xFF
+		header[5] = (timestamp >> 16) & 0xFF
+		header[6] = (timestamp >> 8) & 0xFF
+		header[7] = timestamp & 0xFF
+		# synchronization source (SSRC) identifier
+		header[8] = (ssrc >> 24) & 0xFF
+		header[9] = (ssrc >> 16) & 0xFF
+		header[10] = (ssrc >> 8) & 0xFF
+		header[11] = ssrc & 0xFF
+		# Save header in the object
+		self.header = header
 		
 		# Get the payload from the argument
-		# self.payload = ...
+		self.payload = payload
 		
 	def decode(self, byteStream):
 		"""Decode the RTP packet."""
